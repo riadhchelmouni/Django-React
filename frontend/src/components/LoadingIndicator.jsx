eac9839d7385c59e1ce2cbfffce39c806a34b886
riadhchelmouni <chelmouni.yazidriadh@gmail.com>
2024-11-13 14:11:14 +0100

    first commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,9 @@
+import "../styles/LoadingIndicator.css";
+
+const LoadingIndicator = () => {
+    return <div className="loading-indicator">
+        <div className="loading"></div>
+    </div>;
+};
+
+export default LoadingIndicator;
